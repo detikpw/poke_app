26865d7baf39771d79c4e3748a04a6e8777069fb
Detik <the2ndflamingheart@gmail.com>
2021-02-27 20:22:42 +0700

    [chore] Add / endpoint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 const port = 3000
-app.get('/', (req, res) => res.send('Hello Bro!'))
+app.get('/', function(_, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
