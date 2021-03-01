1315b32c3cb71e872319f9c9a691948a2b25ae02
Detik <the2ndflamingheart@gmail.com>
2021-03-01 18:13:54 +0700

    [chore] Remove unneded enhacnemnet

--- src/client/App.js
+++ src/client/App.js
@@ -1,49 +1,67 @@
-import React from 'react';
-import { ThemeProvider } from 'theme-ui'
-import { ApolloClient, InMemoryCache, HttpLink, ApolloProvider } from '@apollo/client';
-import fetch from 'cross-fetch';
-import { Global } from '@emotion/core'
-import { hashira } from './theme'
-import { Box } from './reusable/flexbox'
-import { Heading } from './reusable/typography';
-import { List } from './pages/pokemon';
-
+import React from "react";
+import { ThemeProvider } from "theme-ui";
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  ApolloProvider,
+} from "@apollo/client";
+import fetch from "cross-fetch";
+import { Global } from "@emotion/core";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { hashira } from "./theme";
+import { Box } from "./reusable/flexbox";
+import { Heading } from "./reusable/typography";
+import { List } from "./pages/pokemon";
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'https://graphql-pokeapi.vercel.app/api/graphql', fetch }),
-  cache: new InMemoryCache()
+  link: new HttpLink({
+    uri: "https://graphql-pokeapi.vercel.app/api/graphql",
+    fetch,
+  }),
+  cache: new InMemoryCache(),
 });
 
 const App = () => (
   <ApolloProvider client={client}>
-    <ThemeProvider theme={hashira}>
-      <Global
-        styles={theme => ({
-          body: {
-            backgroundColor: theme.colors['bg-1'],
-            color: theme.colors.primary,
-            fontFamily: theme.fonts.vt323
-          }
-        })}
-      />
-      <Box
-        px={4}
-        py={2}
-        bg="bg-2"
-        sx={{
-          top: 0,
-          position: 'sticky'
-        }}
-      >
-      <Heading color="alt-2">
-        PokeApp
-      </Heading>
-      </Box>
-      <Box p={4}>
-        <List />
-      </Box>
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={hashira}>
+        <Global
+          styles={(theme) => ({
+            body: {
+              backgroundColor: theme.colors["bg-1"],
+              color: theme.colors.primary,
+              fontFamily: theme.fonts.vt323,
+            },
+          })}
+        />
+        <Box
+          px={4}
+          py={2}
+          bg="bg-2"
+          sx={{
+            top: 0,
+            position: "sticky",
+          }}
+        >
+          <Link to="/">
+            <Heading color="alt-2">PokeApp</Heading>
+          </Link>
+        </Box>
+          <Box p={4}>
+            <Link to="/users">About</Link>
+            <Switch>
+              <Route path="/users">
+                test
+              </Route>
+              <Route path="/">
+                <List />
+              </Route>
+            </Switch>
+          </Box>
+      </ThemeProvider>
+    </Router>
   </ApolloProvider>
 );
 
-export default App
+export default App;
